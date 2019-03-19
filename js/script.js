1683d4177d2fb30af4bd20c8c7e7f89773673ee1
Darius <39289947+dariushill@users.noreply.github.com>
2019-03-19 01:02:33 -0400

    comments added to javascript file

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
 
+//array of objects
+
 var quotes = [
   {quote:"You gain strength, courage, and confidence by every experience in which you really stop to look fear in the face. You are able to say to yourself, 'I lived through this horror. I can take the next thing that comes along.'", source: "Elearnor Rooevelt" , citation: "Speech", year:"1962", tags: "Strength quote"},
   {quote:"Strength and growth come only through continuous effort and struggle.", source: "Napoleon Hill", citation: "speech", year:"1970", tags: "strength quote"},
@@ -7,43 +9,49 @@ var quotes = [
   {quote:"I like criticism. It makes you strong.", source: "LeBron James", citation: " Speech", year:"2016", tags: "Strength quote"}
 ];
 
+
+//random qute function
 function getRandomQuote () {
-  var randomQuote = quotes[Math.floor(Math.random() * quotes.length)];
+  var randomQuote = quotes[Math.floor(Math.random() * quotes.length)]; // random quote expression
   return randomQuote;
 }
+
+// random color function
  function colorGenerator() {
-  return randomColor = Math.floor(Math.random() * 256)
+  return randomColor = Math.floor(Math.random() * 256) // random quote expression
  }
 
+// print function
 function printQuote () {
 
-  setTimeout(printQuote, 1000);
+  setTimeout(printQuote, 1000); // random quote/color time set. 1 sec
    var x = colorGenerator();
    var y = colorGenerator();
    var z = colorGenerator();
    var color = "rgb(" + x + "," + y + "," + z + ")";
 
-  var generateRandomQuote = getRandomQuote();
-  var html = '';
+  var generateRandomQuote = getRandomQuote(); // random number function stored in variable
+  var html = ''; // empty html variable string
   html += '<p class = "quote">' +  generateRandomQuote.quote + '</p>' + '<p class = "source">' + generateRandomQuote.source
 
+  // conditional statement checking  citation property
   if (generateRandomQuote.citation){
     html += '<span class = "citation">' + generateRandomQuote.citation + '</span>';
   }
 
-
+  // conditional statement checking year property
     if (generateRandomQuote.year){
       html += '<span class = "citation">' + generateRandomQuote.year + '</span>';
     }
 
-
+    // conditional statement checking tags property
     if (generateRandomQuote.tags){
       html += '<span class = "tags">'+  generateRandomQuote.tags; + '</span>';
   }
 
     html += '</p>';
-    document.getElementById('quote-box').innerHTML = html;
-    document.body.style.backgroundColor = color;
+    document.getElementById('quote-box').innerHTML = html; // print html string
+    document.body.style.backgroundColor = color; // print random color
   }
 
 document.getElementById('loadQuote').addEventListener("click", printQuote, false);
